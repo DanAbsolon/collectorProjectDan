5b31f469c35341b685b6dc7c85cc959439ba87c0
Dan Abs <danabsolonefl@gmail.com>
2023-02-08 12:20:14 +0000

    responding to review, new images

--- index.php
+++ index.php
@@ -4,22 +4,26 @@ require 'src/db.php';
 
 require 'src/functions.php';
 
+$booksDb = connectToDb('booksDb');
+
+$books = getAllBooks($booksDb);
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en" lang="vi">
+<html lang="en">
 <head>
-	<title>Bookshelf</title>
+    <title>Bookshelf</title>
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://kit.fontawesome.com/5139af5f3f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div tabindex='0'><h1>a bookshelf</h1></div>
+    <h1 tabindex='0'>a bookshelf</h1>
     <?php echo displayBooks($books);
     ?>
-    <p></p>
+    <br id="extra line to prevent display touching end of page"/>
 </body>
 </html>
 
--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,5 @@
 <?php
 
-$booksDb = connectToDb('booksDb');
-
-$books = getAllBooks($booksDb);
-
 function displayBooks($books) {
     $htmlOutput = ''; 
     foreach ($books as $book) {
@@ -12,33 +8,35 @@ function displayBooks($books) {
         } else {
             $possessiveMarker = "'s";
         }    
+        $image = $book['extra'];
         $link = $book['link'];
         $htmlOutput .= "<section tabindex='0'>";
         $htmlOutput .= "<h2>" . $book['bookTitle'] . "</h2>";
         $htmlOutput .= "<p tabindex='0' class='quote'>&#8220" . $book['keyQuote'] . "&#8221</p>";
         $htmlOutput .= "<p tabindex='0'><span class='bookTitle'>" . $book['bookTitle'] 
-            . "</span> is a " 
-            . $book['textType'] 
-            . " of the "
-            . $book['genre']
-            . " genre"
-            . ", written by "
-            . $book['authorFirstName'] 
-            . " "
-            . $book['authorSecondName'] 
-            . " in " 
-            . $book['languageWrittenIn'] 
-            . " and published in "
-            . $book['yearPublished'] 
-            . ". "
-            . $book['authorSecondName']
-            . $possessiveMarker 
-            . " inspiration for the "
-            . $book['textType'] 
-            . " was "
-            . $book['authorInspiration'] 
-            . ".</p>"
-            . "<a class='goodreadsLink' href=$link target='_blank'><i class='fab fa-goodreads'></i></a>";
+                    . "</span> is a " 
+                    . $book['textType'] 
+                    . " of the "
+                    . $book['genre']
+                    . " genre"
+                    . ", written by "
+                    . $book['authorFirstName'] 
+                    . " "
+                    . $book['authorSecondName'] 
+                    . " in " 
+                    . $book['languageWrittenIn'] 
+                    . " and published in "
+                    . $book['yearPublished'] 
+                    . ". "
+                    . $book['authorSecondName']
+                    . $possessiveMarker 
+                    . " inspiration for the "
+                    . $book['textType'] 
+                    . " was "
+                    . $book['authorInspiration'] 
+                    . ".</p>"
+                    . "<img src=$image alt='author image'>"
+                    . "<p><a class='goodreadsLink' href=$link target='_blank'><i class='fab fa-goodreads'></i></a></p>";
         $htmlOutput .= "</section>";
     }
     return $htmlOutput;
